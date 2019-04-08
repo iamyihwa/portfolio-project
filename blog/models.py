9db6acc4e778949c8d5ac15d3cac86fe46184eb3
iamyihwa <iamyihwa@gmail.com>
2019-04-08 16:40:29 +0200

    connected database to the front end page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create A Blog models
+# Add the Blog app to the settings
+# Create a migration
+# Migrate
+# Add to the admin
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length = 255, default = 'No title')
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to = 'images/')
+#    summary = models.CharField(max_length = 200)
